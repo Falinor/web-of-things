b313567838be7b5c2c092d199d6260caecb1ba21
Andrea Gueugnaut <agueugnaut@octo.com>
2017-04-10 19:22:37 +0200

    feat(service): update base service; add barrel files

--- dashboard/src/app/shared/service.ts
+++ dashboard/src/app/shared/service.ts
@@ -1,11 +1,35 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/throw';
+
+const API = '/api';
 
 export class Service<T> {
 
-  constructor(protected http: Http) {}
+  constructor(protected http: Http, protected endpoint: string) {}
 
-  findAll() {
-    // this.http.get()
+  findAll(): Observable<T[]> {
+    return this.http.get(`${API}/${this.endpoint}`)
+      .map(this.json)
+      .catch(this.error);
+  }
+
+  findById(id: number | string): Observable<T> {
+    return this.http.get(`${API}/${this.endpoint}/${id}`)
+      .map(this.json)
+      .catch(this.error);
+  }
+
+  private error(err: Error): any {
+    return Observable.throw(err);
+  }
+
+  private json(res: Response): T | T[] {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    return res.json() || {};
   }
 
 }
