184abf88edb4c3067fe1d002ece58a88e517ab15
Andrea Gueugnaut <agueugnaut@octo.com>
2017-04-11 18:10:50 +0200

    feat(routing): add routesg

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './core';
+
 const routes: Routes = [
-  { path: 'users', loadChildren: './user/user.module#UserModule' },
+  {
+    path: 'users',
+    loadChildren: './user/user.module#UserModule',
+    canActivate: [AuthGuard]
+  },
   { path: 'login', loadChildren: './auth/auth.module#AuthModule' },
   { path: '**', loadChildren: './error/error.module#ErrorModule' }
 ];
@@ -11,4 +17,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- dashboard/src/app/dashboard/dashboard-routing.module.ts
+++ dashboard/src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../core';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
