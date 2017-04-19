1939bdf4c9655ab14a7bc452d4eef34ef283b5c5
Andrea Gueugnaut <agueugnaut@octo.com>
2017-04-19 10:18:55 +0200

    fix(config): remove dotenv loading because env vars are loaded by docker

--- api/src/config.js
+++ api/src/config.js
@@ -10,15 +10,6 @@ const requireProcessEnv = (name) => {
   return process.env[name];
 };
 
-/* istanbul ignore next */
-if (process.env.NODE_ENV !== 'production') {
-  const dotenv = require('dotenv-safe');
-  dotenv.load({
-    path: path.join(__dirname, '../.env'),
-    sample: path.join(__dirname, '../.env.example'),
-  });
-}
-
 const config = {
   all: {
     env: process.env.NODE_ENV || 'development',
