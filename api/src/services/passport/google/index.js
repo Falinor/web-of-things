64a444e09fd0791bcff3f97d3ad013a5283bc9d2
Andrea Gueugnaut <agueugnaut@octo.com>
2017-04-24 16:29:23 +0200

    feat(services): remove unused services; add passport google

---
+++ api/src/services/passport/google/index.js
@@ -0,0 +1,30 @@
+import passport from 'passport';
+
+import User from '../../../api/user/model';
+import config from '../../../config';
+
+import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
+
+passport.use('google', new GoogleStrategy({
+  clientID: config.providers.google.clientId,
+  clientSecret: config.providers.google.clientSecret,
+  callbackURL: `${config.ip}:${config.port}/auth/google/callback`,
+}, (accessToken, refreshToken, profile, done) => {
+  User.createFromService(profile)
+    .then((user) => {
+      done(null, user);
+      return null;
+    })
+    .catch(done);
+}));
+
+/**
+ * Authenticate using Google
+ */
+export const google = (withScopes = false) => {
+  const opts = { session: false };
+  if (withScopes) {
+    opts.scope = ['openid profile email'];
+  }
+  return passport.authenticate('google', opts);
+};
